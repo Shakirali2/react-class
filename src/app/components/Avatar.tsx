19d3e20a145f0ccc7e0badba9de8317b5d6b95c2
Shakirali2 <iamshakir3@gmail.com>
2024-04-03 00:51:45 +0500

    JavaScript in JSX with Curly Braces

---
+++ src/app/components/Avatar.tsx
@@ -0,0 +1,5 @@
+export default function Avatar() {
+    const avatar = "https://i.imgur.com/MK3eW3Am.jpg";
+    const description = "Katherine Johnson";
+    return <img src= {avatar} alt={description} />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Avatar from "./components/Avatar";
 import Gallery from "./components/Gallery";
 import { Profile } from "./components/Gallery";
+import ToDo from "./components/ToDo";
+import Avatar from "./components/Avatar";
 
 export default function Home() {
   return (
     <div>
-      <Gallery />
-      <Profile />
+      {/* <Gallery /> */}
+      {/* <ToDo /> */}
+      <Avatar />
     </div>
   );
 }
