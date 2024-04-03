b56c6c138c3d62aa3ccdf76bc9b5fc325aa74be1
Shakirali2 <iamshakir3@gmail.com>
2024-04-03 23:55:28 +0500

    Using curly braces:

--- src/app/components/ToDoList.tsx
+++ src/app/components/ToDoList.tsx
@@ -1,9 +1,17 @@
 // Using curly braces: A window into the JavaScript world
 
+const today = new Date();
+
+function formateDate(date: Date) {
+    return new Intl.DateTimeFormat("en-US", { weekday : "long"}).format(date);
+}
+
 export default function TodoList() {
     const name = 'Gregorio Y. Zara';
     return (
-      <h1>{name}'s To Do List</h1>
+      <h1>
+        {name}'s To Do List {formateDate(today)}
+        </h1>
     );
   }
   
