d10c2dc6eb74a619ee09058da3ca9fa9dddfe184
Shakirali2 <iamshakir3@gmail.com>
2024-04-05 00:21:47 +0500

    Step 1: Pass props to the child component

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,4 +1,14 @@
-export default function Avatar() {
+interface AvatarType {
+    person : Person;
+    size: number;
+}
+
+interface Person {
+    name: string;
+    imageId : string
+}
+
+export default function Avatar({ person, size }: AvatarType) {
     const avatar = "https://i.imgur.com/MK3eW3Am.jpg";
     const description = "Katherine Johnson";
     return <img src= {avatar} alt={description} />;
