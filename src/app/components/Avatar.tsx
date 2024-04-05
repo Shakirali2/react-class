4c4ebb23389d8d8bb9c6580f8efd31a0ecb81609
Shakirali2 <iamshakir3@gmail.com>
2024-04-06 00:51:06 +0500

    Step 2: Read props inside the child component

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -10,7 +10,12 @@ interface Person {
 
 export default function Avatar({ person, size }: AvatarType) {
     const name = person.name;
-    const avatar =`https://i.imgur.com/${person.imageId}.jpg`
-    const description = "Katherine Johnson";
-    return <><h1>{name}</h1><img src= {avatar} alt={name} /></>;
+    const avatar =`https://i.imgur.com/${person.imageId}.jpg`;
+    // const description = "Katherine Johnson";
+    return(
+    <>
+    <h1>{name}</h1>
+    <img src= {avatar} alt={name} />
+    </>
+    ); 
 } 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Gallery from "./components/Gallery";
-import { Profile } from "./components/Gallery";
+// import { Profile } from "./components/Gallery";
 import ToDo from "./components/ToDo";
 import Avatar from "./components/Avatar";
 import TodoList from "./components/ToDoList";
+import Profile from "./components/Profile";
 
 export default function Home() {
   return (
     <div>
       {/* <Gallery /> */}
       {/* <ToDo /> */}
-      <Avatar />
-      <TodoList />
+      <Profile />
     </div>
   );
 }
