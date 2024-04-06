52759eaa2d153a5342bbc93eb6b0797bf9b820f3
Shakirali2 <iamshakir3@gmail.com>
2024-04-06 23:38:23 +0500

    Conditional (ternary) operator (? :)

--- src/app/components/PackingList.tsx
+++ src/app/components/PackingList.tsx
@@ -1,8 +1,5 @@
 function Item({ name, isPacked }: any) {
-    if (isPacked) {
-        return <li className="item">{name} ✔</li>;
-      }
-      return <li className="item">{name}</li>;
+      return <li className="item">{isPacked ? name + ' ✔' : name}</li>;
   }
   
   export default function PackingList() {
