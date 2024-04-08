5dfe860fad70b21d036e520d992ea9dc4b0b17ab
Shakirali2 <iamshakir3@gmail.com>
2024-04-08 23:44:32 +0500

    Rendering Lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import Avatar from "./components/Avatar";
 import TodoList from "./components/ToDoList";
 import Profile from "./components/Profile";
 import PackingList from "./components/PackingList";
+import List from "./components/List";
 
 export default function Home() {
   return (
     <div>
+      <List />
       {/* <Gallery /> */}
       {/* <ToDo /> */}
       {/* <Profile /> */}
-      <PackingList />
+      {/* <PackingList /> */}
     </div>
   );
 }
