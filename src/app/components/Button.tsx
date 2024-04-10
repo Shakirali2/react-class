cb4c2004d94884f430f148a501a16ae6472ea120
Shakirali2 <iamshakir3@gmail.com>
2024-04-10 23:59:58 +0500

    Button

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,8 +1,15 @@
+"use Client";
+
 export default function Button() {
+    function handleClick() {
+        alert("The button was clicked!");
+    }
     return (
-      <button>
-        I don't do anything
-      </button>
+        <>
+      <button>I don't do anything</button>
+      <br />
+      <button onClick={handleClick}>I do something</button>
+        </>
     );
   }
   
